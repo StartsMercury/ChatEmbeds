00ee1143b2bb8bd0f008c67a78471daf80aaa68c
StartsMercury <startsmercury@gmail.com>
2022-02-20 11:28:54 +0800

    Handle null jsons in util.Jsons
     - no more NullPointerException when loading config

--- src/main/java/com/yunus1903/chatembeds/util/Jsons.java
+++ src/main/java/com/yunus1903/chatembeds/util/Jsons.java
@@ -5,18 +5,42 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 
 public final class Jsons {
+	public static boolean getAsBooleanOrDefault(final JsonElement json, final boolean defaultValue) {
+		if (json instanceof final JsonPrimitive jsonPrimitive) {
+			return unsafelyGetAsBooleanOrDefault(jsonPrimitive, defaultValue);
+		} else {
+			return defaultValue;
+		}
+	}
+
 	public static boolean getAsBooleanOrDefault(final JsonPrimitive jsonPrimitive, final boolean defaultValue) {
-		return jsonPrimitive.isBoolean() ? jsonPrimitive.getAsBoolean() : defaultValue;
+		if (jsonPrimitive != null) {
+			return unsafelyGetAsBooleanOrDefault(jsonPrimitive, defaultValue);
+		} else {
+			return defaultValue;
+		}
+	}
+
+	public static int getAsIntOrDefault(final JsonElement json, final int defaultValue) {
+		if (json instanceof final JsonPrimitive jsonPrimitive) {
+			return unsafelyGetAsIntOrDefault(jsonPrimitive, defaultValue);
+		} else {
+			return defaultValue;
+		}
 	}
 
 	public static int getAsIntOrDefault(final JsonPrimitive jsonPrimitive, final int defaultValue) {
-		return jsonPrimitive.isNumber() ? jsonPrimitive.getAsInt() : defaultValue;
+		if (jsonPrimitive != null) {
+			return unsafelyGetAsIntOrDefault(jsonPrimitive, defaultValue);
+		} else {
+			return defaultValue;
+		}
 	}
 
 	public static boolean getBooleanOrDefault(final JsonElement json, final String memberName,
 			final boolean defaultValue) {
 		if (json instanceof final JsonObject jsonObject) {
-			return getBooleanOrDefault(jsonObject, memberName, defaultValue);
+			return unsafelyGetBooleanOrDefault(jsonObject, memberName, defaultValue);
 		} else {
 			return defaultValue;
 		}
@@ -24,26 +48,48 @@ public final class Jsons {
 
 	public static boolean getBooleanOrDefault(final JsonObject jsonObject, final String memberName,
 			final boolean defaultValue) {
-		if (jsonObject.get(memberName) instanceof final JsonPrimitive jsonPrimitive) {
-			return getAsBooleanOrDefault(jsonPrimitive, defaultValue);
-		} else {
-			return defaultValue;
-		}
+		return jsonObject != null ? unsafelyGetBooleanOrDefault(jsonObject, memberName, defaultValue) : defaultValue;
 	}
 
 	public static int getIntOrDefault(final JsonElement json, final String memberName, final int defaultValue) {
 		if (json instanceof final JsonObject jsonObject) {
-			return getIntOrDefault(jsonObject, memberName, defaultValue);
+			return unsafelyGetIntOrDefault(jsonObject, memberName, defaultValue);
 		} else {
 			return defaultValue;
 		}
 	}
 
 	public static int getIntOrDefault(final JsonObject jsonObject, final String memberName, final int defaultValue) {
-		if (jsonObject.get(memberName) instanceof final JsonPrimitive jsonPrimitive) {
-			return getAsIntOrDefault(jsonPrimitive, defaultValue);
+		return jsonObject != null ? unsafelyGetIntOrDefault(jsonObject, memberName, defaultValue) : defaultValue;
+	}
+
+	@Deprecated
+	public static boolean unsafelyGetAsBooleanOrDefault(final JsonPrimitive jsonPrimitive, final boolean defaultValue) {
+		if (jsonPrimitive.isBoolean()) {
+			return jsonPrimitive.getAsBoolean();
 		} else {
 			return defaultValue;
 		}
+	}
+
+	@Deprecated
+	public static int unsafelyGetAsIntOrDefault(final JsonPrimitive jsonPrimitive, final int defaultValue) {
+		if (jsonPrimitive.isNumber()) {
+			return jsonPrimitive.getAsInt();
+		} else {
+			return defaultValue;
+		}
+	}
+
+	@Deprecated
+	public static boolean unsafelyGetBooleanOrDefault(final JsonObject jsonObject, final String memberName,
+			final boolean defaultValue) {
+		return getAsBooleanOrDefault(jsonObject.get(memberName), defaultValue);
+	}
+
+	@Deprecated
+	public static int unsafelyGetIntOrDefault(final JsonObject jsonObject, final String memberName,
+			final int defaultValue) {
+		return getAsIntOrDefault(jsonObject.get(memberName), defaultValue);
 	}
 }
